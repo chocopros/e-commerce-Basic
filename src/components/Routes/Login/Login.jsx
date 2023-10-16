6fd1a3cc54addf9de19480237015563c49d5c16d
jesuschock93 <jesuschock93@gmail.com>
2023-10-16 16:13:03 -0400

    localStore

--- src/components/Routes/Login/Login.jsx
+++ src/components/Routes/Login/Login.jsx
@@ -1,8 +1,31 @@
+import axios from 'axios'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
 
 const Login = () => {
+
+  const {register, handleSubmit, reset} = useForm();
+
+  const submit = data => {
+
+    //console.log(data)
+    const URL = `https://e-commerce-api-v2.academlo.tech/api/v1/users/login`
+
+
+
+    axios.post(URL, data)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+
+
+    reset({
+      email: '',
+      password: ''
+    })
+  }
+
   return (
     <MainLogin>
       <FormLogin>
@@ -14,15 +37,28 @@ const Login = () => {
           <p><span><i className="fa-solid fa-lock fa-beat"></i></span>john1234</p>
         </TestData>
 
-        <FormData>
+        <FormData onSubmit={handleSubmit(submit)}>
           <div>
             <h3>Email</h3>
-            <input type="text" />
+
+            <input 
+              {...register('email')} 
+              type="email" 
+              id='email'
+            />
+
           </div>
           <div>
             <h3>Password</h3>
-            <input type="password" />
+
+            <input 
+              {...register('password')}
+              type="password"
+              id='password' 
+            />
+
           </div>
+
           <button>Login</button>
         </FormData>
 
